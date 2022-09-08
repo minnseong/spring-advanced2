2cee44615e7c491e7079f32fe1fc89aed5aafffb
minseong <kdsvip5@naver.com>
2022-09-09 01:53:18 +0900

    Initial commit & spring bean example

---
+++ src/main/java/hello/proxy/app/v2/OrderRepositoryV2.java
@@ -0,0 +1,19 @@
+package hello.proxy.app.v2;
+
+public class OrderRepositoryV2 {
+
+    public void save(String itemId) {
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int millions) {
+        try {
+            Thread.sleep(millions);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
